dac00d43c653866e2a4ff0fed8dd51b2fd67c274
AsrafuzzamanKhan <asrafuzzamankhan89@gmail.com>
2024-03-16 15:46:24 +0600

    singlePage

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,16 +5,16 @@ import "leaflet/dist/leaflet.css"
 import Pin from '../Pin/Pin'
 
 const Map = ({ items }) => {
-    const position = [51.505, -0.09]
+    // const position = [51.505, -0.09]
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} className='map'>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className='map'>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {items.map(item => <Pin item={item} key={item.id}></Pin>)}
+            {items.map(item => (<Pin item={item} key={item.id}></Pin>))}
         </MapContainer>
     )
 }
 
-export default Map
+export default Map;
